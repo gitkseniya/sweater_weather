2aa09d0844c3413876c777710e6edf355a7c9878
Kseniya K <kseniya.kudelich@gmail.com>
2021-06-13 23:25:16 -0600

    Add filter config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,7 +79,8 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.filter_sensitive_data('<api_key>') { ENV['api_key'] }
+  config.filter_sensitive_data('<mapquest_key>') { ENV['mapquest_key'] }
+  config.filter_sensitive_data('<openweather_key>') { ENV['openweather_key'] }
   # config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata!
 end
