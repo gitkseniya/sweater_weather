71f86cc9d9ab9f8bfaa1eb6ca98a632a36cf69e8
Kseniya K <kseniya.kudelich@gmail.com>
2021-06-14 11:58:05 -0600

    Add tests for a specific book

--- spec/features/book_search_spec.rb
+++ spec/features/book_search_spec.rb
@@ -16,8 +16,20 @@ RSpec.describe 'books API', type: :request do
       expect(books).to be_a(Hash)
       expect(books[:id]).to eq(nil)
       expect(books[:type]).to eq('books')
-      expect(books[:attributes]).to be_an(Hash)
+      expect(books[:attributes]).to be_a(Hash)
       expect(books[:attributes].keys).to eq([:destination, :forecast, :total_books_found, :books])
+
+      book_list = books[:attributes][:books]
+      expect(book_list).to be_an(Array)
+      expect(book_list.size).to eq(3)
+      expect(book_list.first).to be_a(Hash)
+      expect(book_list.first.keys).to eq([:isbn, :title, :publisher])
+
+      first_book = book_list.first
+      expect(first_book[:isbn]).to be_an(Array)
+      expect(first_book[:title]).to eq("Mobil Travel Guide")
+      expect(first_book[:publisher]).to be_an(Array)
+      expect(first_book[:publisher].first).to eq("Mobil Travel Guide")
     end
   end
 
