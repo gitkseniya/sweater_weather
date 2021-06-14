311b630e2be55d46fdb11e6e9316b86a5fd10bbd
Kseniya K <kseniya.kudelich@gmail.com>
2021-06-14 13:21:22 -0600

    Add sad path

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::SearchController < ApplicationController
 
   def show
-    coordinates = LocationFacade.get_lat_lon(params[:location])
-    weather = WeatherFacade.get_details(coordinates.lat, coordinates.lon)
+    if params[:location] && !params[:location].empty?
+      coordinates = LocationFacade.get_lat_lon(params[:location])
+      weather = WeatherFacade.get_details(coordinates.lat, coordinates.lon)
 
-    render json: ForecastSerializer.new(weather)
+      render json: ForecastSerializer.new(weather)
+    else
+      invalid_params
+    end
   end
 end
