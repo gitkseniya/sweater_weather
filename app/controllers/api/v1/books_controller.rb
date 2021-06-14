dcc78f32e5408b967787927415c977dfc1984c08
Kseniya K <kseniya.kudelich@gmail.com>
2021-06-14 11:49:05 -0600

    Add sad paths

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,12 +1,23 @@
 class Api::V1::BooksController < ApplicationController
   def index
-    coordinates = LocationFacade.get_lat_lon(params[:location])
-    weather = WeatherFacade.get_details(coordinates.lat, coordinates.lon)
+    if params[:quantity].to_i > 0 && params[:location] && real_location(params[:location])
+      coordinates = LocationFacade.get_lat_lon(params[:location])
+      weather = WeatherFacade.get_details(coordinates.lat, coordinates.lon)
 
-    destination = params[:location]
+      destination = params[:location]
 
-    books = BooksFacade.get_books(destination, params[:quantity], weather)
+      books = BooksFacade.get_books(destination, params[:quantity], weather)
 
-    render json: BooksSerializer.new(books)
+      render json: BooksSerializer.new(books)
+    else
+      invalid_params
+    end
+  end
+
+  private
+
+  def real_location(location)
+    return false if BooksService.get_books(location).empty?
+    true
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def invalid_params
+    render json: { data: {}, error: 'invalid parameters'}, status: 400
+  end 
 end
