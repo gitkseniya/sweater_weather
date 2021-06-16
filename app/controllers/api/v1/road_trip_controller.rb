46fc26fa450f96d882ea748ed27602edc1c65cc9
Kseniya K <kseniya.kudelich@gmail.com>
2021-06-15 21:52:44 -0600

    Add sad path for road trip

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -5,9 +5,7 @@ class Api::V1::RoadTripController < ApplicationController
     elsif roadtrip_params[:api_key].empty?
       render json: { error: 'API key is missing' }, status: 401
     else
-      @trip = RoadTripFacade.get_trip_details(roadtrip_params[:origin], roadtrip_params[:destination])
-
-      render json: RoadTripSerializer.new(@trip), status: 201
+      trip_results
     end
   end
 
@@ -15,5 +13,14 @@ class Api::V1::RoadTripController < ApplicationController
 
   def roadtrip_params
     params.permit(:origin, :destination, :api_key)
+  end
+
+  def trip_results
+    @trip = RoadTripFacade.get_trip_details(roadtrip_params[:origin], roadtrip_params[:destination])
+      if @trip == 'You are not going there buddy'
+        render json: { error: @trip }, status: 400
+      else
+        render json: RoadTripSerializer.new(@trip), status: 201
+      end
   end
 end
--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -3,10 +3,14 @@ class RoadTripFacade
   def self.get_trip_details(origin, destination)
     trip = RoadTripService.get_trip_details(origin, destination)
 
-    location_data = LocationFacade.get_lat_lon(destination)
+    if trip[:info][:statuscode] == 402
+      return 'You are not going there buddy'
+    else
+      location_data = LocationFacade.get_lat_lon(destination)
 
-    weather = WeatherFacade.get_details(location_data.lat, location_data.lon)
+      weather = WeatherFacade.get_details(location_data.lat, location_data.lon)
 
-    RoadTrip.new(trip, weather, origin, destination)
+      RoadTrip.new(trip, weather, origin, destination)
+    end
   end
 end
